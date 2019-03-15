3441d309d30534c57c5dbd2d6222570a28789f9f
Robert <rmrowiec@icloud.com>
2019-03-15 18:52:50 +0100

    tests

--- tests/acceptance/ember-tutorial-test.js
+++ tests/acceptance/ember-tutorial-test.js
@@ -1,19 +1,27 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { click, currentURL, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | ember tutorial', function(hooks) {
   setupApplicationTest(hooks);
 
   test('should show rentals as the home page', async function (assert) {
+    await visit('/')
+    assert.equal(currentURL(), '/rentals', 'should automatically redirect')
   });
 
   test('should link to information about the company.', async function (assert) {
+    await visit('/')
+    await click('.menu-about')
+    assert.equal(currentURL(), '/about', 'should navigate to about page')
   });
 
-  test('should link to contact information.', async function (assert) {
+  test('should link to contact information', async function(assert) {
+    await visit('/')
+    await click('.menu-contact')
+    assert.equal(currentURL(), '/contact', 'should navigate to contact page')
   });
-
+  
   test('should list available rentals.', async function (assert) {
   });
 
@@ -25,7 +33,6 @@ module('Acceptance | ember tutorial', function(hooks) {
 
   test('visiting /', async function(assert) {
     await visit('/');
-
     assert.equal(currentURL(), '/');
   });
 });
